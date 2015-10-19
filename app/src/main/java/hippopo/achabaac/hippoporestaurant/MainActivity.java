4924fb03f2114b18581b1baa14372866ba3cfccf
hippopo <acha.baac@gmail.com>
2015-10-19 16:04:00 +0700

    Finish Day 1

--- app/src/main/java/hippopo/achabaac/hippoporestaurant/MainActivity.java
+++ app/src/main/java/hippopo/achabaac/hippoporestaurant/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Explicit ประกาศตัวแปร
     private UserTABLE objUserTABLE;
+    private FoodTABLE objFoodTABLE; //ตัวแปรสีเทา (ยังไม่ใช้งาน) สีม่วง (ใช้งวานแล้ว)
 
 
     @Override
@@ -17,11 +18,23 @@ public class MainActivity extends AppCompatActivity {
         //create & connected Database
         createAndConnected();    //Method ที่ทำหน้าที่แค่ Create และ Connected Database
 
+        //Tester Add New Value
+        testerAdd();
+
+
     } // นี่คือ main Method
+
+    private void testerAdd(){
+        objUserTABLE.addNewUser("testUser", "testPassword","ทดสอบภาษาไทย");
+        objFoodTABLE.addNewFood("ชื่ออาหาร", "testSource","123");
+    }
+
+
 
     private void createAndConnected() {
 
         objUserTABLE = new UserTABLE(this); // this เรียกใช้ UserTABLE อัตโนมัติ
+        objFoodTABLE = new FoodTABLE(this);
 
     }
 }   //นี่คือ Main Class
