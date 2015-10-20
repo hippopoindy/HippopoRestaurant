37409712da9e7b493ac217d91f081dca252d3b12
hippopo <acha.baac@gmail.com>
2015-10-20 09:52:37 +0700

    Delete All Data

--- app/src/main/java/hippopo/achabaac/hippoporestaurant/MainActivity.java
+++ app/src/main/java/hippopo/achabaac/hippoporestaurant/MainActivity.java
@@ -1,5 +1,6 @@
 package hippopo.achabaac.hippoporestaurant;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -19,15 +20,24 @@ public class MainActivity extends AppCompatActivity {
         createAndConnected();    //Method ที่ทำหน้าที่แค่ Create และ Connected Database
 
         //Tester Add New Value
-        testerAdd();
+        //testerAdd();
+
+        //Delete All SQLite
+        deleteAllSQLite();
 
 
     } // นี่คือ main Method
 
+    private void deleteAllSQLite() {
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase("BAAC.db", MODE_PRIVATE, null); //MODE_PRIVATE ลบ data เฉยๆ ตารางคงไว้
+        objSqLiteDatabase.delete("userTABLE", null, null);
+        objSqLiteDatabase.delete("foodTABLE", null, null);
+    }
+
     private void testerAdd(){
         objUserTABLE.addNewUser("testUser", "testPassword","ทดสอบภาษาไทย");
         objFoodTABLE.addNewFood("ชื่ออาหาร", "testSource","123");
-    }
+    } // method ไว้ใช้ test เพิ่มข้อมูลเข้า DB
 
 
 
