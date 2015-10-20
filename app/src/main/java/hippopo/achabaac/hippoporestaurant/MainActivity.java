0a29a3ba24f4ff0c535570883d18087f6a7b19e1
hippopo <acha.baac@gmail.com>
2015-10-20 12:12:32 +0700

    syn JSON to SQLite

--- app/src/main/java/hippopo/achabaac/hippoporestaurant/MainActivity.java
+++ app/src/main/java/hippopo/achabaac/hippoporestaurant/MainActivity.java
@@ -1,8 +1,23 @@
 package hippopo.achabaac.hippoporestaurant;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Switch;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,10 +38,120 @@ public class MainActivity extends AppCompatActivity {
         //testerAdd();
 
         //Delete All SQLite
-        deleteAllSQLite();
+       deleteAllSQLite();
+
+
+        // synchronize JSON to SQLite
+       synJSONtoSQLite();
 
 
     } // นี่คือ main Method
+
+    private void synJSONtoSQLite() {
+        //0. Change Policy
+        // เปิด Policy ทุกตัวให้สามารถใช้งานได้หมด
+        StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(myPolicy);   //ฉันสมารถเชื่อมต่อกับ Protocol ได้แล้ว
+
+
+        int intTime = 0;
+        while (intTime <= 1) {
+
+            InputStream objInputStream = null; // โหลดไปทำงานไป
+            String strJSON = null;
+            String strUserURL = "http://swiftcodingthai.com/baac/php_get_data_master.php";
+            String strFoodURL = "http://swiftcodingthai.com/baac/php_get_food.php";
+            HttpPost objHttpPost;       // build tool version 22.0.1 (Project Structure)
+
+
+            //1. Create InputStream
+            // JSON Array
+            try {
+
+                HttpClient objHttpClient = new DefaultHttpClient();
+
+                switch(intTime){
+                    case 0:
+                        objHttpPost = new HttpPost(strUserURL); // case 0 ดึง User
+                        break;
+                    case 1:
+                        objHttpPost = new HttpPost(strFoodURL); // case 1 ดึง Food
+                        break;
+                    default:
+                        objHttpPost = new HttpPost(strUserURL); // บังคับค่า default เมื่อ case ไม่เท่ากับ 0 หรือ 1
+                        break;
+                }   //Switch
+
+                HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+                HttpEntity objHttpEntity = objHttpResponse.getEntity();
+                objInputStream = objHttpEntity.getContent();
+
+
+            } catch (Exception e) {
+                Log.d("BAAC", "InputStream ==> " + e.toString());
+
+            }
+
+            //2. create JSON String
+            try {
+
+                BufferedReader objBufferedReader = new BufferedReader(new InputStreamReader(objInputStream, "UTF-8"));
+                StringBuilder objStringBuilder = new StringBuilder();
+                String strLine = null;
+
+                while ((strLine = objBufferedReader.readLine()) != null ) {
+
+                    objStringBuilder.append(strLine);
+
+                }   //while
+
+                objInputStream.close();
+                strJSON = objStringBuilder.toString();
+
+            }catch (Exception e) {
+                Log.d("BAAC", "StrJSON ==>" + e.toString());
+            }
+
+            //3. Update SQlite
+            try {
+
+                JSONArray objJsonArray = new JSONArray(strJSON);
+
+                for (int i = 0; i < objJsonArray.length(); i++) {
+
+                    JSONObject object = objJsonArray.getJSONObject(i);
+
+                    switch (intTime) {
+                        case 0:
+                            // For UserTABLE
+                            String strUser = object.getString("User");
+                            String strPassword = object.getString("Password");
+                            String strName = object.getString("Name");
+                            objUserTABLE.addNewUser(strUser, strPassword, strName);
+
+                            break;
+                        case 1:
+                            // For FoodTABLE
+                            String strFood = object.getString("Food");
+                            String strSource = object.getString("Source");
+                            String strPrice = object.getString("Price");
+                            objFoodTABLE.addNewFood(strFood, strSource, strPrice);
+
+                            break;
+                    }   //switch
+
+                } //for
+
+            } catch (Exception e) {
+                Log.d("BAAC", "Update ==>" + e.toString());
+            }
+
+            intTime += 1;  //******
+
+        } //while
+
+
+    }   // method synJSONtoSQLite
 
     private void deleteAllSQLite() {
         SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase("BAAC.db", MODE_PRIVATE, null); //MODE_PRIVATE ลบ data เฉยๆ ตารางคงไว้
